f1efa97f3b12b5b029a56a94d5a28a1a1ded285a
ssp <ssp@huskers.unl.edu>
2019-10-19 13:16:31 -0500

    Added remove function from items

--- src/main/java/edu/unl/cse/csce361/gui_prototype/Controller.java
+++ src/main/java/edu/unl/cse/csce361/gui_prototype/Controller.java
@@ -1,4 +1,19 @@
 package edu.unl.cse.csce361.gui_prototype;
 
+import java.awt.event.ActionEvent;
+
 public class Controller {
+	
+	public int noOfItems = 0;
+	
+	public void removeButton(ActionEvent event) {
+		
+		if(noOfItems == 0) {
+			noOfItems = 0;
+		}
+		else {
+			noOfItems--;
+		}
+	}
+	
 }
