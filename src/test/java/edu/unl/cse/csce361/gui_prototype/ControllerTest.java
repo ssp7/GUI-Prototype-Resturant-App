5d5a8e513753b5ad7bb7dda0e08d4cca1382655e
ssp <ssp@huskers.unl.edu>
2019-10-19 12:11:57 -0500

    Added test case for Controller and FXMLLoader

---
+++ src/test/java/edu/unl/cse/csce361/gui_prototype/ControllerTest.java
@@ -0,0 +1,14 @@
+package edu.unl.cse.csce361.gui_prototype;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/javafx/fxml/FXMLLoaderTest.java
@@ -0,0 +1,14 @@
+package javafx.fxml;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FXMLLoaderTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
